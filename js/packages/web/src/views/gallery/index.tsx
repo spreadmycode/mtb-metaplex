29e8c33ada277e676bb6972e55ac2504973b7501
spreadmycode <spreadmycode@outlook.com>
2021-12-01 16:03:55 -0800

    filter engine fixed

--- js/packages/web/src/views/gallery/index.tsx
+++ js/packages/web/src/views/gallery/index.tsx
@@ -22,6 +22,7 @@ const GetAllMutation = gql`
 
 export const GalleryView = () => {
     const history = useHistory();
+    const [allNfts, setAllNfts] = useState<any>([]);
     const [nfts, setNfts] = useState<any>([]);
     const [background, setBackground] = useState('');
     const [faction, setFaction] = useState('');
@@ -30,6 +31,11 @@ export const GalleryView = () => {
     const [sequence, setSequence] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [getAll] = useMutation(GetAllMutation);
+    const [backgrounds, setBackgrounds] = useState<Array<string>>([]);
+    const [factions, setFactions] = useState<Array<string>>([]);
+    const [types, setTypes] = useState<Array<string>>([]);
+    const [generations, setGenerations] = useState<Array<string>>([]);
+    const [sequences, setSequences] = useState<Array<string>>([]);
 
     useEffect(() => {
         (async () => {
@@ -37,11 +43,54 @@ export const GalleryView = () => {
 
             const metadatasMut = await getAll();
             const metadatas = metadatasMut.data.getAll.metadatas;
-            console.log(metadatas);
+
+            let tbackgrounds = new Array<string>();
+            let tfactions = new Array<string>();
+            let ttypes = new Array<string>();
+            let tgenerations = new Array<string>();
+            let tsequences = new Array<string>();
             const nfts = metadatas?.map((data: any) => {
-              return {pubkey: data.pubkey, metadata: JSON.parse(data.attributes)};
-            })
+                const nft = {pubkey: data.pubkey, metadata: JSON.parse(data.attributes)};
+                const attributes = nft.metadata.attributes;
+                for (let i = 0; i < attributes.length; i++) {
+                    const attribute = attributes[i];
+                    if (attribute.trait_type == 'Background') {
+                        if (!tbackgrounds.includes(attribute.value)) {
+                            tbackgrounds.push(attribute.value);
+                        }
+                    }
+                    if (attribute.trait_type == 'Faction') {
+                        if (!tfactions.includes(attribute.value)) {
+                            tfactions.push(attribute.value);
+                        }
+                    }
+                    if (attribute.trait_type == 'Type') {
+                        if (!ttypes.includes(attribute.value)) {
+                            ttypes.push(attribute.value);
+                        }
+                    }
+                    if (attribute.trait_type == 'Generation') {
+                        if (!tgenerations.includes(attribute.value)) {
+                            tgenerations.push(attribute.value);
+                        }
+                    }
+                    if (attribute.trait_type == 'Sequence') {
+                        if (!tsequences.includes(attribute.value)) {
+                            tsequences.push(attribute.value);
+                        }
+                    }
+                }
+                
+                return nft;
+            });
             setNfts(nfts);
+            setAllNfts(nfts);
+
+            setBackgrounds(tbackgrounds);
+            setFactions(tfactions);
+            setTypes(ttypes);
+            setGenerations(tgenerations);
+            setSequences(tsequences);
 
             setIsLoading(false);
         })();
@@ -80,6 +129,7 @@ export const GalleryView = () => {
     );
 
     const handleChange = (_value: string, _type: string) => {
+
         let searchParmas = [
             {type: 'Background', value: background},
             {type: 'Faction', value: faction},
@@ -87,8 +137,14 @@ export const GalleryView = () => {
             {type: 'Generation', value: generation},
             {type: 'Sequence', value: sequence},
         ];
+
+        for (let i = 0; i < searchParmas.length; i++) {
+            if (searchParmas[i].type == _type) {
+                searchParmas[i].value = _value;
+            }
+        }
         
-        setNfts(nfts.filter((nft: any) => {
+        setNfts(allNfts.filter((nft: any) => {
             let searchResult = [
                 {type: 'Background', value: false},
                 {type: 'Faction', value: false},
@@ -98,35 +154,35 @@ export const GalleryView = () => {
                 {type: 'Input', value: false}
             ]
 
-            let attrs = nft.metadata.attributes;
+            const attrs = nft.metadata.attributes;
             for (let j = 0; j < attrs.length; j++) {
-              const attr = attrs[j];
-              for (let k = 0; k < searchParmas.length; k++) {
-                  let searchParam = searchParmas[k];
-                  if (searchParam.type == attr.trait_type) {
-                      if (searchParam.value == '') {
-                          for (let l = 0; l < searchResult.length; l++) {
-                              if (searchResult[l].type == attr.trait_type) {
-                                  searchResult[l].value = true;
-                              }
-                          }
-                      } else if (searchParam.value == attr.value) {
-                          for (let l = 0; l < searchResult.length; l++) {
-                              if (searchResult[l].type == attr.trait_type) {
-                                  searchResult[l].value = true;
-                              }
-                          }
-                      }
-                  }
-              }
+                const attr = attrs[j];
+                for (let k = 0; k < searchParmas.length; k++) {
+                    let searchParam = searchParmas[k];
+                    if (searchParam.type == attr.trait_type) {
+                        if (searchParam.value == '') {
+                            for (let l = 0; l < searchResult.length; l++) {
+                                if (searchResult[l].type == attr.trait_type) {
+                                    searchResult[l].value = true;
+                                }
+                            }
+                        } else if (searchParam.value == attr.value) {
+                            for (let l = 0; l < searchResult.length; l++) {
+                                if (searchResult[l].type == attr.trait_type) {
+                                    searchResult[l].value = true;
+                                }
+                            }
+                        }
+                    }
+                }
 
-              if (_type == attr.trait_type) {
-                  if (_value == '') {
-                      searchResult[5].value = true;
-                  } else if (_value == attr.value) {
-                      searchResult[5].value = true;
-                  }
-              } 
+                if (_type == attr.trait_type) {
+                    if (_value == '') {
+                        searchResult[5].value = true;
+                    } else if (_value == attr.value) {
+                        searchResult[5].value = true;
+                    }
+                } 
             }
             let flag = true;
             for (let i = 0; i < searchResult.length; i++) {
@@ -147,61 +203,59 @@ export const GalleryView = () => {
             </Button>
             <Divider orientation="left">Filter by attributes</Divider>
             <Row justify="center" align="middle">
-                <Col xl={4} xs={24} style={{margin: "10"}}>
-                    <label>Background Type:</label>
+                <Col xl={4} xs={24} style={{margin: "10", display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <label style={{ width: 150, textAlign: 'center' }}>Background :</label>
                     <Select placeholder="Background" style={{ width: 150 }} onChange={(value: string) => {setBackground(value); handleChange(value, 'Background');}}>
                         <Option value="">All</Option>
-                        <Option value="Yello">Yello</Option>
-                        <Option value="Red">Red</Option>
-                        <Option value="Blue">Blue</Option>
+                        {
+                            backgrounds.map(data => {
+                                return <Option key={data} value={data}>{data}</Option>;
+                            })
+                        }
                     </Select>
                 </Col>
-                <Col xl={4} xs={24} style={{margin: 10}}>
-                    <label>Faction Type:</label>
+                <Col xl={4} xs={24} style={{margin: "10", display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <label style={{ width: 150, textAlign: 'center' }}>Faction :</label>
                     <Select placeholder="Faction" style={{ width: 150 }} onChange={(value: string) => {setFaction(value); handleChange(value, 'Faction');}}>
                         <Option value="">All</Option>
-                        <Option value="Surrealist">Surrealist</Option>
-                        <Option value="Celestial">Celestial</Option>
+                        {
+                            factions.map(data => {
+                                return <Option key={data} value={data}>{data}</Option>;
+                            })
+                        }
                     </Select>
                 </Col>
-                <Col xl={4} xs={24} style={{margin: 10}}>
-                    <label>Gender Type:</label>
+                <Col xl={4} xs={24} style={{margin: "10", display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <label style={{ width: 150, textAlign: 'center' }}>Gender :</label>
                     <Select placeholder="Type" style={{ width: 150 }} onChange={(value: string) => {setType(value); handleChange(value, 'Type');}}>
                         <Option value="">All</Option>
-                        <Option value="Male">Male</Option>
-                        <Option value="Female">Female</Option>
+                        {
+                            types.map(data => {
+                                return <Option key={data} value={data}>{data}</Option>;
+                            })
+                        }
                     </Select>
                 </Col>
-                <Col xl={4} xs={24} style={{margin: 10}}>
-                    <label>Generation:</label>
+                <Col xl={4} xs={24} style={{margin: "10", display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <label style={{ width: 150, textAlign: 'center' }}>Generation:</label>
                     <Select placeholder="Generation" style={{ width: 150 }} onChange={(value: string) => {setGeneration(value); handleChange(value, 'Generation');}}>
                         <Option value="">All</Option>
-                        <Option value="1">1</Option>
-                        <Option value="2">2</Option>
-                        <Option value="3">3</Option>
-                        <Option value="4">4</Option>
-                        <Option value="5">5</Option>
-                        <Option value="6">6</Option>
-                        <Option value="7">7</Option>
-                        <Option value="8">8</Option>
-                        <Option value="9">9</Option>
-                        <Option value="10">10</Option>
+                        {
+                            generations.map(data => {
+                                return <Option key={data} value={data}>{data}</Option>;
+                            })
+                        }
                     </Select>
                 </Col>
-                <Col xl={4} xs={24} style={{margin: 10}}>
-                    <label>Sequence:</label>
+                <Col xl={4} xs={24} style={{margin: "10", display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <label style={{ width: 150, textAlign: 'center' }}>Sequence:</label>
                     <Select placeholder="Sequence" style={{ width: 150 }} onChange={(value: string) => {setSequence(value); handleChange(value, 'Sequence');}}>
                         <Option value="">All</Option>
-                        <Option value="1">1</Option>
-                        <Option value="2">2</Option>
-                        <Option value="3">3</Option>
-                        <Option value="4">4</Option>
-                        <Option value="5">5</Option>
-                        <Option value="6">6</Option>
-                        <Option value="7">7</Option>
-                        <Option value="8">8</Option>
-                        <Option value="9">9</Option>
-                        <Option value="10">10</Option>
+                        {
+                            sequences.map(data => {
+                                return <Option key={data} value={data}>{data}</Option>;
+                            })
+                        }
                     </Select>
                 </Col>
             </Row>
