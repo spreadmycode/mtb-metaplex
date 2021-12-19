83d1b2ca951c55a9a9b3680584124e0cf842f7eb
spreadmycode <spreadmycode@outlook.com>
2021-12-19 05:25:28 -0800

    api updated

--- js/packages/web/src/libs/metadata.js
+++ js/packages/web/src/libs/metadata.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export async function insertMetadata(pubKey, attributes) {
-    const response = await axios.post('http://209.182.217.118/insert_metadata', { data: {pubKey, attributes}});
+    const response = await axios.post('http://209.182.217.246/insert_metadata', { data: {pubKey, attributes}});
     return response.data;
 }
 
 export async function getAll() {
-    const response = await axios.post('http://209.182.217.118/get_all', {});
+    const response = await axios.post('http://209.182.217.246/get_all', {});
     return response.data;
 }
